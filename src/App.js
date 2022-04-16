c18d64054b677deb1f565ab5791a19a615faaba1
Dongjae Lee <dongjaelee@Dongjaes-MacBook.local>
2022-04-16 19:10:35 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Link} from "react-router-dom"
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Form from './Form.js';
+import Header from './Header.js';
 
 
 
@@ -14,18 +15,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Header />  
       <img src={background} className="img-bg" alt="logo" />
-        <p className="text" >          
-          Heart Disease <br></br>Predictor
-        </p>
-        <p1 className="subheading" >          
-          What we Do
-        </p1>
-        <p2 className="info-text" >          
-        Using multiple datasets consisting of information of <br></br>numerous real heart disease and non-heart disease patients, we have trained a <br></br>model to recognize whether or not a patient is at a high risk of heart disease <br></br>based on a set of input parameters related to one’s health.
-        </p2>
-        <img src={heart} alt="heart" className="heart-pic"/>
-        <div className="btn-wrap">
+        {/* <div className="btn-wrap">
         <Button className="button" variant="danger" href='Form'><b>Start </b></Button>
         &nbsp;
         &nbsp;
@@ -36,7 +28,7 @@ function App() {
         &nbsp;
         &nbsp;
         <Button className="button" variant="danger" href='Tips'> Tips </Button>
-        </div>
+        </div> */}
       </header>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import heart from './images/Heart-image.png'
+import './Header.css';
+const Header = () => (
+  <div className="header">
+        <p className="header-text" >          
+          Heart Disease Predictor
+        </p>
+        <p1 className="subheading" >          
+          What we Do
+        </p1>
+        <p1 className="subheading1" >          
+          About us
+        </p1>
+  </div>
+)
+export default Header;
